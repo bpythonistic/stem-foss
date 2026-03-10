6b793ef5cc7bf710d653e81c37c7d0bbff0b40c8
Byron <sixthbaron9@protonmail.com>
2026-03-10 09:11:58 -0400

    STEM-10 Pre-commit changes

--- backend/app/features/target_mechanics.py
+++ backend/app/features/target_mechanics.py
@@ -77,9 +77,7 @@ def generate_random_target(target_class: TargetClass, user_id: str) -> Target:
             raise ValueError(f"Invalid target class: {target_class}")
 
 
-def generate_map_heat_points(
-    target_map: Map, margin: float = 5.00
-) -> pl.DataFrame:
+def generate_map_heat_points(target_map: Map, margin: float = 5.00) -> pl.DataFrame:
     """
     Generate random heat points for the map.
 
@@ -126,12 +124,8 @@ def describe_lanes(target_map: Map, heat_points: pl.DataFrame) -> pl.DataFrame:
             "lane_id": pl.arange(0, target_map.num_heat_points),
             "x_start": heat_points["x"],
             "y_start": heat_points["y"],
-            "x_end": heat_points["x"]
-            .shift(-1)
-            .fill_null(heat_points["x"].first()),
-            "y_end": heat_points["y"]
-            .shift(-1)
-            .fill_null(heat_points["y"].first()),
+            "x_end": heat_points["x"].shift(-1).fill_null(heat_points["x"].first()),
+            "y_end": heat_points["y"].shift(-1).fill_null(heat_points["y"].first()),
             "traffic_density": npr.uniform(
                 0, 1, target_map.num_heat_points
             ),  # Random traffic density
@@ -158,9 +152,7 @@ def map_lane_traffic(
     x_values = pl.linear_space(0, target_map.map_size, target_map.resolution)
     y_values = pl.linear_space(0, target_map.map_size, target_map.resolution)
 
-    lane_indices = pl.Series(
-        "lane_idx", pl.arange(0, target_map.num_heat_points)
-    )
+    lane_indices = pl.Series("lane_idx", pl.arange(0, target_map.num_heat_points))
     for i in lane_indices:
         q1 = pl.DataFrame(
             {
@@ -187,8 +179,7 @@ def map_lane_traffic(
                 lanes[i, "traffic_density"]
                 * np.exp(
                     -0.5
-                    * (pl.col("dist_to_lane") ** 2 / lanes[i, "traffic_stddev"])
-                    ** 2
+                    * (pl.col("dist_to_lane") ** 2 / lanes[i, "traffic_stddev"]) ** 2
                 )
             ).alias("traffic")
         ).lazy()
